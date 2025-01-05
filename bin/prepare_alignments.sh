f03688b0b4db62fe6abcccc073d04dcd5ed37ad0
Fabian Ackle <fabian.ackle@gmail.com>
2025-01-05 17:28:39 +0100

    Renamed bams.

--- bin/prepare_alignments.sh
+++ bin/prepare_alignments.sh
@@ -12,7 +12,7 @@ for reads_file in clusters/*.fastq.gz; do
     uuid=$(basename "$reads_file" .fastq.gz)
  
     reference_file="references/${uuid}.fasta"
-    output_bam="alignments/calls_to_draft_fc${uuid}"
+    output_bam="alignments/cluster_${uuid}"
 
     echo "Alligning reads for flycode: $uuid."
     mini_align.sh -i $reads_file -r $reference_file -m -p $output_bam -t $threads
