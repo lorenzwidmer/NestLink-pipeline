6680a2a7247261640073e12db0c109841634f1a9
Fabian Ackle <fabian.ackle@gmail.com>
2025-03-25 13:14:50 +0100

    Removed functionality for orf2 variant calling.

--- bin/variant_calling.py
+++ bin/variant_calling.py
@@ -7,19 +7,18 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 
 
-def extract_sequences(file_path, flycode, orf1, orf2=None):
+def extract_sequences(file_path, flycode, orf):
     """
-    Parse a FASTA file to extract flycode, ORF1, and optional ORF2 sequences.
+    Parse a FASTA file to extract flycode and ORF sequences.
 
     Args:
         file_path (str): Path to the FASTA file.
         flycode (tuple): A (start_pattern, end_pattern) tuple for the flycode.
-        orf1 (tuple): A (start_pattern, end_pattern) tuple for ORF1.
-        orf2 (tuple, optional): A (start_pattern, end_pattern) tuple for ORF2 (default: None).
+        orf (tuple): A (start_pattern, end_pattern) tuple for the ORF.
 
     Returns:
         dict: Keys are cluster IDs; values are tuples of extracted sequences 
-              (flycode_seq, orf1_seq, [orf2_seq if provided]) in-frame only.
+              (flycode_seq, orf_seq) in-frame only.
     """
     def is_in_frame(start_match, end_match):
         return (end_match.start() - start_match.end()) % 3 == 0
@@ -32,12 +31,8 @@ def extract_sequences(file_path, flycode, orf1, orf2=None):
     fc_start = re.compile(flycode[0], re.IGNORECASE)
     fc_end = re.compile(flycode[1], re.IGNORECASE)
 
-    orf1_start = re.compile(orf1[0], re.IGNORECASE)
-    orf1_end = re.compile(orf1[1], re.IGNORECASE)
-
-    if orf2:
-        orf2_start = re.compile(orf2[0], re.IGNORECASE)
-        orf2_end = re.compile(orf2[1], re.IGNORECASE)
+    orf_start = re.compile(orf[0], re.IGNORECASE)
+    orf_end = re.compile(orf[1], re.IGNORECASE)
 
     with dnaio.open(file_path, mode="r") as reader:
         for record in reader:
@@ -48,27 +43,17 @@ def extract_sequences(file_path, flycode, orf1, orf2=None):
             fc_start_match = fc_start.search(sequence)
             fc_end_match = fc_end.search(sequence)
 
-            # Search for orf1
-            orf1_start_match = orf1_start.search(sequence)
-            orf1_end_match = orf1_end.search(sequence)
-
-            # Search for orf2
-            if orf2:
-                orf2_start_match = orf2_start.search(sequence)
-                orf2_end_match = orf2_end.search(sequence)
+            # Search for orf
+            orf_start_match = orf_start.search(sequence)
+            orf_end_match = orf_end.search(sequence)
 
             # Extract sequences if matches are found and in-frame
             if fc_start_match and fc_end_match and is_in_frame(fc_start_match, fc_end_match):
                 flycode_sequence = extract_subsequence(sequence, fc_start_match, fc_end_match)
 
-                if orf1_start_match and orf1_end_match and is_in_frame(orf1_start_match, orf1_end_match):
-                    orf1_sequence = extract_subsequence(sequence, orf1_start_match, orf1_end_match)
-
-                    if orf2 and orf2_start_match and orf2_end_match and is_in_frame(orf2_start_match, orf2_end_match):
-                        orf2_sequence = extract_subsequence(sequence, orf2_start_match, orf2_end_match)
-                        sequences[cluster_id] = (flycode_sequence, orf1_sequence, orf2_sequence)
-                    else:
-                        sequences[cluster_id] = (flycode_sequence, orf1_sequence)
+                if orf_start_match and orf_end_match and is_in_frame(orf_start_match, orf_end_match):
+                    orf_sequence = extract_subsequence(sequence, orf_start_match, orf_end_match)
+                    sequences[cluster_id] = (flycode_sequence, orf_sequence)
     return sequences
 
 
@@ -111,17 +96,16 @@ def compare_aa_sequences(sequence, reference):
     return changes
 
 
-def get_aa_changes(sequences, reference, orf2=None):
+def get_aa_changes(sequences, reference):
     """
-    Determine amino acid changes for each cluster ID relative to a the reference.
+    Determine amino acid changes for each consensus sequence relative to a the reference.
 
     Args:
-        sequences (dict): Dictionary mapping cluster IDs to extracted (flycode, ORF1, [ORF2]) sequences.
+        sequences (dict): Dictionary mapping cluster IDs to extracted (flycode, ORF) sequences.
         reference (dict): Dictionary containing exactly one reference entry of the same format.
-        orf2 (str): Name of the second ORF if present, otherwise None.
 
     Returns:
-        dict: Maps each cluster ID to a tuple of (flycode_aa, ORF1_changes, [ORF2_changes]).
+        dict: Maps each cluster ID to a tuple of (flycode_aa, ORF_changes).
     """
     aa_changes = {}
 
@@ -131,57 +115,39 @@ def get_aa_changes(sequences, reference, orf2=None):
             "The reference file does not contain exactly one record."
         )
     _, reference_sequence = next(iter(reference.items()))
-    reference_orf1_aa = translate(reference_sequence[1])
-    if orf2:
-        reference_orf2_aa = translate(reference_sequence[2])
+    reference_orf_aa = translate(reference_sequence[1])
 
     # Looping through all sequences
     for cluster_id, values in sequences.items():
         flycode_aa = translate(values[0])
-        orf1_aa = translate(values[1])
-        orf1_aa_changes = compare_aa_sequences(orf1_aa, reference_orf1_aa)
-        if orf2:
-            orf2_aa = translate(values[2])
-            orf2_aa_changes = compare_aa_sequences(orf2_aa, reference_orf2_aa)
-            aa_changes[cluster_id] = (flycode_aa, orf1_aa_changes, orf2_aa_changes)
-        else:
-            aa_changes[cluster_id] = (flycode_aa, orf1_aa_changes)
+        orf_aa = translate(values[1])
+        orf_aa_changes = compare_aa_sequences(orf_aa, reference_orf_aa)
+        aa_changes[cluster_id] = (flycode_aa, orf_aa_changes)
     return aa_changes
 
 
-def write_flycode_db(aa_changes, output_path, prefix, orf1, orf2=None):
+def write_flycode_db(aa_changes, output_path, prefix, orf):
     """
     Write a flycode database in FASTA format, grouping identical variants.
 
     Args:
-        aa_changes (dict): A dictionary of cluster IDs to (flycode_aa, ORF1_changes, [ORF2_changes]).
+        aa_changes (dict): A dictionary of cluster IDs to (flycode_aa, ORF_changes).
         output_path (str): Path to the output FASTA file.
         prefix (str): Prefix (e.g., experiment name) to include in FASTA headers.
-        orf1 (str): Name of ORF1.
-        orf2 (str, optional): Name of ORF2 if present, otherwise None.
+        orf (str): Name of ORF.
     """
     variants = defaultdict(list)
 
     for cluster_id, values in aa_changes.items():
         flycode = values[0]
-        if orf2:
-            orf_changes = tuple(values[1]), tuple(values[2])   # both orfs
-        else:
-            orf_changes = tuple(values[1])  # only orf1
+        orf_changes = tuple(values[1])
         variants[orf_changes].append(flycode)
 
     with open(output_path, "w") as file:
-        if orf2:
-            for variant, flycodes in variants.items():
-                variant_orf1 = ",".join(variant[0])
-                variant_orf2 = ",".join(variant[1])
-                file.write(f">{prefix}|{orf1}={variant_orf1};{orf2}={variant_orf2}\n")
-                file.write(f"{''.join(flycodes)}\n")
-        else:
-            for variant, flycodes in variants.items():
-                variant_orf1 = ",".join(variant)
-                file.write(f">{prefix}|{orf1}={variant_orf1}\n")
-                file.write(f"{''.join(flycodes)}\n")
+        for variant, flycodes in variants.items():
+            variant_orf = ",".join(variant)
+            file.write(f">{prefix}|{orf}={variant_orf}\n")
+            file.write(f"{''.join(flycodes)}\n")
 
 
 def main(args):
